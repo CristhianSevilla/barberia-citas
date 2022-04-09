05a859819a5e385dcac5fabb62f29e0eaef53d85
Cristhian Sevilla <crissev17@gmail.com>
2022-04-09 15:07:12 -0500

    Estructura de MVC y Sass Agregados

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+require_once __DIR__ . '/../includes/app.php';
+
+use MVC\Router;
+
+$router = new Router();
+
+//Iniciar Sesión
+
+
+
+
+// Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
+$router->comprobarRutas();
